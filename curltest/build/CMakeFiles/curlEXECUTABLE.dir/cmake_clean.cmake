d245e6debdffc0942e63e4e82f8d48335a90bd8c
DOM <740199085@qq.com>
2020-10-12 14:36:36 +0800

    update daily

---
+++ curltest/build/CMakeFiles/curlEXECUTABLE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curlEXECUTABLE.dir/curl-new.c.o"
+  "curlEXECUTABLE.pdb"
+  "curlEXECUTABLE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/curlEXECUTABLE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
